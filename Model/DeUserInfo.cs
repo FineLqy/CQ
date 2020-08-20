c5a2e810e6b1f7920644b3641aa8d85f60231446
FineLqy <773865484@qq.com>
2020-08-20 09:24:53 +0800

    修改

---
+++ Model/DeUserInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public class DeUserInfo
+    {
+        public DeUserInfo()
+        {
+            UserName = "";
+            UserPwd = "";
+            OrderNumber = "";
+
+        }
+        public int ID { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string UserPwd { get; set; }
+        /// <summary>
+        /// 订单号
+        /// </summary>
+        public string OrderNumber { get; set; }
+    }
+}
